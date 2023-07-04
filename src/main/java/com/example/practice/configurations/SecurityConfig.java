9d8b9619b30ce8ced9df30ea4dcef4078a5ea475
Prashant Chavan <prashantch451@gmail.com>
2023-07-04 12:29:45 +0530

    code refactored: SecurityConfig.java file is moved to the seperate package

--- src/main/java/com/example/practice/configurations/SecurityConfig.java
+++ src/main/java/com/example/practice/configurations/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.practice;
+package com.example.practice.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
