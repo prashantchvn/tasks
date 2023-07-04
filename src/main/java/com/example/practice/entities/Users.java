ec6c1b143fcf181f0f27183ae1881eddd36c6ec8
Prashant Chavan <prashantch451@gmail.com>
2023-07-04 18:01:39 +0530

    password is now hidden from all the services

--- src/main/java/com/example/practice/entities/Users.java
+++ src/main/java/com/example/practice/entities/Users.java
@@ -1,9 +1,7 @@
 package com.example.practice.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 class Address{
