9ba0d02728bd636cf77166527aea19f374ce8160
Prashant Chavan <prashantch451@gmail.com>
2023-07-04 13:48:43 +0530

    AdminInterceptor and SellerInterceptor is added

---
+++ src/main/java/com/example/practice/interceptors/SellerInterceptor.java
@@ -0,0 +1,26 @@
+package com.example.practice.interceptors;
+
+import com.example.practice.entities.Users;
+import com.example.practice.repository.UserRepository;
+import com.example.practice.utilities.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class SellerInterceptor implements HandlerInterceptor {
+    private final JwtUtils utils;
+    @Autowired
+    private UserRepository userRepository;
+
+    public SellerInterceptor(JwtUtils utils) {
+        this.utils = utils;
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        String token = request.getHeader("Authorization").substring(7);
+        String email = utils.getUsernameFromToken(token);
+        Users authenticatedUser = userRepository.findByEmail(email);
+        return authenticatedUser.getSeller();
+    }
+}
