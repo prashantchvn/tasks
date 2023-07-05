834d1f6c80695183900cf26fd26a0a54a3249508
Prashant Chavan <prashantch451@gmail.com>
2023-07-05 11:52:38 +0530

    Query import is removed from the UserRepository.java

--- src/main/java/com/example/practice/repository/UserRepository.java
+++ src/main/java/com/example/practice/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.practice.repository;
 
 import com.example.practice.entities.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
